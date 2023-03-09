f5f87740ec2e865f2bcabb2b9f6e39d419d65946
HamidMasood479 <hamidmasood479@gmail.com>
2023-03-09 17:08:15 +0500

    JS signin

--- Learning/Lab 3 9-3-23/jsscript.js
+++ Learning/Lab 3 9-3-23/jsscript.js
@@ -1,1 +1,18 @@
-console.log("Hello from external script");
+function checkLogin() {
+  var username = document.getElementById("username");
+  var password = document.getElementById("password");
+  var button = document.getElementById("loginbtn");
+  if (username.value && password.value) {
+    username.classList.remove("error");
+    password.classList.remove("error");
+    username.classList.add("success");
+    password.classList.add("success");
+    button.classList.remove("errorbutton");
+  } else {
+    username.classList.remove("success");
+    password.classList.remove("success");
+    username.classList.add("error");
+    password.classList.add("error");
+    button.classList.add("errorbutton");
+  }
+}
